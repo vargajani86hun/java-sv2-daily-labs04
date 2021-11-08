eb657c998ad2cf54dbf884295bf4009a54029762
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-08 21:45:48 +0100

    Minimum and maximum selection is outsourced to private methods.

--- day03/src/main/java/day03/Stock.java
+++ day03/src/main/java/day03/Stock.java
@@ -10,18 +10,28 @@ public class Stock {
     }
 
     public double maxProfit() {
+        int minimum = getMinimumIndex();
+        int maximum = getMaximumIndexFromStart(minimum);
+        return prices.get(maximum) - prices.get(minimum);
+    }
+
+    private int getMinimumIndex() {
         int minimum = 0;
         for (int i = 1; i < prices.size(); i++) {
             if (prices.get(minimum) > prices.get(i)) {
                 minimum = i;
             }
         }
-        int maximum = minimum;
-        for (int i = minimum + 1; i < prices.size(); i++) {
+        return minimum;
+    }
+
+    private int getMaximumIndexFromStart(int startIndex) {
+        int maximum = startIndex;
+        for (int i = startIndex + 1; i < prices.size(); i++) {
             if (prices.get(i) > prices.get(maximum)) {
                 maximum = i;
             }
         }
-        return prices.get(maximum) - prices.get(minimum);
+        return maximum;
     }
 }
